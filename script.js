58ba1219af892679d42abe4c5d4a95910c217697
Zachary Tan | 陳銘哲 <zia.pubacc@gmail.com>
2021-05-14 23:25:20 +0800

    add index style script

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings: ["Font-End Developer","Web Developer","UI Designer","SEO Developer","LineBot Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
